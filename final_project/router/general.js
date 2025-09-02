a17ca9f694506365af286d7a4674ec2cf8d6fe1f
seang673 <seangriffiths673@gmail.com>
2025-09-01 21:50:19 -0400

    Key Changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,19 +32,41 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const match = Object.values(books).filter((b) => b.author === author);
+
+  if(match.length > 0){
+    return res.status(200).json(match);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const match = Object.values(books).filter((b) => b.title === title);
+
+  if(match.length > 0){
+    return res.status(200).json(match);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (book){
+    const review = book.review;
+    return res.status(200).json(review);
+  } else {
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
